f0bf2f1bff218e91baaf1b920bbfbc166f1b8586
lhnaseeb63 <lhnaseeb@gmail.com>
2022-09-12 13:13:31 +0300

    fixed textarea expanding beyond grid bounds

--- Frontend/NAW_Grid/styleScript.js
+++ Frontend/NAW_Grid/styleScript.js
@@ -62,10 +62,12 @@ buttons.forEach((btn) => {
 
 // -------------------------------------------------------------Code to make textarea expand with input
 const textarea = document.querySelector('textarea');
+const textareaGrid = document.querySelector('.notesField');
 textarea.addEventListener('keyup', (e) => {
   textarea.style.height = 'auto';
   let scHeight = e.target.scrollHeight;
   console.log(scHeight);
 
   textarea.style.height = `${scHeight}px`;
+  textareaGrid.style.height = `${scHeight}px`;
 });
