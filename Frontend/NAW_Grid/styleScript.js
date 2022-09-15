7c0a93b26981dc7d3585fdc74c3d64c4ee745c3c
lhnaseeb63 <lhnaseeb@gmail.com>
2022-09-15 11:35:09 +0300

    NavBar transitions and SVG fixed

--- Frontend/NAW_Grid/styleScript.js
+++ Frontend/NAW_Grid/styleScript.js
@@ -76,14 +76,18 @@ textarea.addEventListener('keyup', (e) => {
 let sidebar = document.querySelector('.sidebar');
 let closeBtn = document.querySelector('#btn');
 let searchBtn = document.querySelector('.bx-search');
+let gridBody = document.querySelector('.gridBody');
+let sidebarOpenSize = 250;
+let sideBarClosedSize = 78;
 
 closeBtn.addEventListener('click', () => {
   sidebar.classList.toggle('open');
   menuBtnChange(); //calling the function(optional)
+  changeGridBodySize();
 });
 
 searchBtn.addEventListener('click', () => {
-  // Sidebar open when you click on the search iocn
+  // Sidebar open when you click on the search icon
   sidebar.classList.toggle('open');
   menuBtnChange(); //calling the function(optional)
 });
@@ -91,8 +95,17 @@ searchBtn.addEventListener('click', () => {
 // following are the code to change sidebar button(optional)
 function menuBtnChange() {
   if (sidebar.classList.contains('open')) {
-    closeBtn.classList.replace('bx-menu', 'bx-menu-alt-right'); //replacing the iocns class
+    closeBtn.classList.replace('bx-menu', 'bx-menu-alt-right'); //replacing the icons class
   } else {
-    closeBtn.classList.replace('bx-menu-alt-right', 'bx-menu'); //replacing the iocns class
+    closeBtn.classList.replace('bx-menu-alt-right', 'bx-menu'); //replacing the icons class
+  }
+}
+
+function changeGridBodySize() {
+  if (sidebar.classList.contains('open')) {
+    gridBody.style.width = `calc(100% - ${sidebarOpenSize}px)`;
+    gridBody.style.transition = 'all 0.5s ease';
+  } else {
+    gridBody.style.width = `calc(100% - ${sideBarClosedSize}px)`;
   }
 }
