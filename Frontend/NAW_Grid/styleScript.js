bc3780fb36d8abca6b67b5bdbfc19bc15321e926
lhnaseeb63 <lhnaseeb@gmail.com>
2022-09-15 13:38:06 +0300

    Date and time now working

--- Frontend/NAW_Grid/styleScript.js
+++ Frontend/NAW_Grid/styleScript.js
@@ -7,7 +7,7 @@ let labelDivArray = [];
 let temp = [];
 let lastClicked;
 
-//-----------------------------------------------------------Code to make labels for clicked field bold
+//------------------------------------------------------------------------- Code to make labels for clicked field bold
 setClassArrays();
 
 // Creating arrays to compare class names
@@ -60,7 +60,7 @@ buttons.forEach((btn) => {
   });
 });
 
-// -------------------------------------------------------------Code to make textarea expand with input
+// --------------------------------------------------------------------------------- Code to make textarea expand with input
 const textarea = document.querySelector('textarea');
 const textareaGrid = document.querySelector('.notesField');
 textarea.addEventListener('keyup', (e) => {
@@ -72,13 +72,20 @@ textarea.addEventListener('keyup', (e) => {
   textareaGrid.style.height = `${scHeight}px`;
 });
 
-//--------------------------------------------------------------Nav Bar Code
+//----------------------------------------------------------------------------------------------------- Nav Bar Code
 let sidebar = document.querySelector('.sidebar');
 let closeBtn = document.querySelector('#btn');
 let searchBtn = document.querySelector('.bx-search');
 let gridBody = document.querySelector('.gridBody');
+let dateEl = document.querySelector('.date');
+let timeEl = document.querySelector('.time');
+
 let sidebarOpenSize = 250;
 let sideBarClosedSize = 78;
+
+// create array for days and months
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
 closeBtn.addEventListener('click', () => {
   sidebar.classList.toggle('open');
@@ -86,13 +93,6 @@ closeBtn.addEventListener('click', () => {
   changeGridBodySize();
 });
 
-searchBtn.addEventListener('click', () => {
-  // Sidebar open when you click on the search icon
-  sidebar.classList.toggle('open');
-  menuBtnChange(); //calling the function(optional)
-});
-
-// following are the code to change sidebar button(optional)
 function menuBtnChange() {
   if (sidebar.classList.contains('open')) {
     closeBtn.classList.replace('bx-menu', 'bx-menu-alt-right'); //replacing the icons class
@@ -109,3 +109,24 @@ function changeGridBodySize() {
     gridBody.style.width = `calc(100% - ${sideBarClosedSize}px)`;
   }
 }
+
+function setTime() {
+  const time = new Date();
+  const month = time.getMonth();
+  const day = time.getDay();
+  const date = time.getDate();
+  const hours = time.getHours();
+  //   We want a 12 hour clock, not 24
+  const hoursForClock = hours % 12;
+  const minutes = time.getMinutes();
+  // const seconds = time.getSeconds();
+  const ampm = hours >= 12 ? 'PM' : 'AM';
+
+  timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes}${ampm}`;
+  dateEl.innerHTML = `${days[day]}, ${months[month]} ${date}`;
+}
+
+setTime();
+
+// calls setTime once every 1000ms
+setInterval(setTime, 1000);
