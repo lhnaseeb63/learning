a0398dac6ee421169f1c399f6d79be73d3aace38
lhnaseeb63 <lhnaseeb@gmail.com>
2022-11-27 10:34:15 +0300

    started reels. 3 in row. basic glow working

--- Frontend/Tim_Portfolio/hover/script.js
+++ Frontend/Tim_Portfolio/hover/script.js
@@ -10,3 +10,30 @@ Array.from(document.getElementsByClassName('menu-item')).forEach((item, index) =
     menu.dataset.activeIndex = index;
   };
 });
+
+// ============================================= Reels hover effect
+// event handler for onmousemove
+const handleOnMouseMove = (e) => {
+  // get card position
+  const { currentTarget: target } = e;
+
+  //   calculate position of mouse relative to each card
+  const rect = target.getBoundingClientRect(),
+    x = e.clientX - rect.left,
+    y = e.clientY - rect.top;
+
+  // with the x & y can set custom css properties
+  target.style.setProperty('--mouse-x', `${x}px`);
+  target.style.setProperty('--mouse-y', `${y}px`);
+};
+
+// add onmousemove event listener to each card
+for (const card of document.querySelectorAll('.card')) {
+  card.onmousemove = (e) => handleOnMouseMove(e);
+}
+
+/**
+ * The Element.getBoundingClientRect() method returns a DOMRect
+ * object providing information about the size of an element
+ * and its position relative to the viewport.
+ */
