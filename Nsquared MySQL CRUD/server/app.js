d79ac21ce12917d969984b1256036700eff15cf3
Laila Hamed Naseeb <lhnaseeb@gmail.com>
2023-01-25 09:50:03 +0300

    need to get xampp working

---
+++ Nsquared MySQL CRUD/server/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const cors =  require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended:false }));
+
+// create
+app.post('/insert', (req, res)=>{
+
+});
+
+// read
+app.get('/getAll', (req, res)=>{
+    console.log('test');
+    res.json({
+        success: true
+    });
+});
+
+// update
+
+// delete
+
+app.listen(process.env.PORT, ()=> console.log('app is running'))
