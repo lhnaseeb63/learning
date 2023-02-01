63879b0a3a20bf642a593789b38380e58a92989c
Laila Hamed Naseeb <lhnaseeb@gmail.com>
2023-02-01 10:30:13 +0300

    adding and deleting work

--- Nsquared MySQL CRUD/client/index.js
+++ Nsquared MySQL CRUD/client/index.js
@@ -1,14 +1,103 @@
 document.addEventListener('DOMContentLoaded', ()=>{
     fetch('http://localhost:5000/getAll')
     .then(res => res.json())
-    .then(data => console.log(data));
-    loadHTMLTable([])
+    .then(data => loadHTMLTable(data['data']));
+    
+});
+
+const addBtn = document.getElementById('add-name-btn');
+
+// Not using React or AJS to send the data to the backend so
+// we are doing it manually. Would normally use something like 
+// ng-model to get the data where it needs to go. 
+addBtn.addEventListener('click', ()=>{
+    const nameInput = document.querySelector('#name-input');
+    const name = nameInput.value;
+    nameInput.value = "";
+
+    fetch('http://localhost:5000/insert', {
+        headers: {
+            'Content-type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify({ name : name})
+    })
+    .then(response => response.json())
+    .then(data => insertRowIntoTable(data['data']));
 })
 
-function loadHTMLTable (data){
+function insertRowIntoTable(data) {
+    console.log(data);
     const table = document.querySelector('table tbody');
-    // let tableHTML = '';
-    if(data.length === 0){
-        table.innerHTML = "<tr><td class='no-data' colspan='5'>No Data</td></tr>";
+    const isTableData = table.querySelector('.no-data');
+
+    let tableHtml = "<tr>";
+
+    // getting an object back, not an array so we use for-in loop
+    for (var key in data){
+        // if the key has the property of date added
+        if(data.hasOwnProperty(key)){
+            if(key === 'dateAdded'){
+                data[key] = new Date(data[key]).toLocaleString();
+            }
+
+            tableHtml += `<td>${data[key]}</td>`
+        }
     }
+
+    tableHtml += `<td><button class="delete-row-btn" data-id=${data.id}>Delete</td>`;
+    tableHtml += `<td><button class="edit-row-btn" data-id=${data.id}>Edit</td>`;
+
+    tableHtml += "</tr>";
+
+    if (isTableData) {
+        table.innerHTML = tableHtml;
+    } else {
+        const newRow = table.insertRow();
+        newRow.innerHTML = tableHtml;
+    }
+}
+
+function loadHTMLTable(data) {
+    const table = document.querySelector('table tbody');
+
+    if (data.length === 0) {
+        table.innerHTML = "<tr><td class='no-data' colspan='5'>No Data</td></tr>";
+        return;
+    }
+
+    let tableHtml = "";
+
+    data.forEach(function ({id, name, date_added}) {
+        tableHtml += "<tr>";
+        tableHtml += `<td>${id}</td>`;
+        tableHtml += `<td>${name}</td>`;
+        tableHtml += `<td>${new Date(date_added).toLocaleString()}</td>`;
+        tableHtml += `<td><button class="delete-row-btn" data-id=${id}>Delete</td>`;
+        tableHtml += `<td><button class="edit-row-btn" data-id=${id}>Edit</td>`;
+        tableHtml += "</tr>";
+    });
+
+    table.innerHTML = tableHtml;
+}
+
+document.querySelector('table tbody').addEventListener('click', (e)=>{
+    if(e.target.className === "delete-row-btn"){
+        // create a new function and send the id of the element
+        // to the backend to delete the row from the database
+        deleteRowById(e.target.dataset.id);
+    }
+})
+
+function deleteRowById(id){
+    fetch('http://localhost:5000/delete/' + id, {
+        method: 'DELETE'
+    })
+    .then(response => response.json())
+    .then(data => {
+        if(data.success){
+            location.reload();
+        }
+        console.log(data)
+    });
 }
--- Nsquared MySQL CRUD/server/app.js
+++ Nsquared MySQL CRUD/server/app.js
@@ -5,25 +5,51 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
+const dbService = require('./dbService');
+const { response } = require('express');
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended:false }));
 
 // create
 app.post('/insert', (req, res)=>{
+    const { name } = req.body;
+    const db = dbService.getDbServiceInstance();
 
+    const result = db.insertNewName(name);
+
+    result
+    .then(data => res.json({data:data}))
+    .catch(err => console.log(err))
 });
 
 // read
 app.get('/getAll', (req, res)=>{
-    console.log('test');
-    res.json({
-        success: true
-    });
+    const db = dbService.getDbServiceInstance();
+    
+    // db.getAllData() returns a promise
+    const result = db.getAllData();
+
+    result
+    .then(data => res.json({ data : data }))
+    .catch(err => console.log(err));
 });
 
 // update
 
 // delete
+app.delete('/delete/:id', (req, res)=>{
+    const { id } = req.params;
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.deleteRowById(id);
+
+    // data in this case will be true or false
+    result
+    .then(data => res.json({ success : data }))
+    .catch(err => console.log(err));
+
+})
 
 app.listen(process.env.PORT, ()=> console.log('app is running'))
