217de65465b82004e032e51b907cd5ae1ee64a0f
Laila Hamed Naseeb <lhnaseeb@gmail.com>
2023-02-09 11:36:57 +0300

    file objects are logged to node console. now to write middleware

---
+++ Upload/daveGray_nodeExpress/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const path = require('path');
+
+const PORT = process.env.PORT || 3500;
+
+const app = express();
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname, "index.html"));
+})
+
+app.post('/upload',
+    fileUpload({ createParentPath: true }),
+    (req,res)=>{
+        const files =  req.files;
+        // nodejs console, not browser console 
+        console.log(files);
+
+        return res.json({ status: 'logged', message: 'logged' })
+    }
+)
+
+
+
+
+
+app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
