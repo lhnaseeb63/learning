e49709ecfb1646fb5fe637b6eedd0631c3b22026
Laila Hamed Naseeb <lhnaseeb@gmail.com>
2023-03-23 12:54:10 +0300

    Made a service to retrieve tasks and first observable

---
+++ Angular CC/angular-cc/src/app/components/tasks-item/tasks-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import {Task} from '../../Task'
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+        
+
+@Component({
+  selector: 'app-tasks-item',
+  templateUrl: './tasks-item.component.html',
+  styleUrls: ['./tasks-item.component.css']
+})
+export class TasksItemComponent {
+  @Input() task: Task;
+  faTimes = faTimes;
+
+}
